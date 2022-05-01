cd0c52536890cc499a9d9b3c65b63cc235bd6609
Jarius Thomas <jarthoma@publicisgroupe.net>
2022-05-01 13:32:32 -0400

    Piping in Train and Test, getting numpy arrays for their inputs etc

--- main.py
+++ main.py
@@ -4,15 +4,39 @@ import time
 
 
 #Handle the input of data from test or train
-def loadData(csv)
+def loadData(filepath, isTrain):
     #ingest a CSV
 
-    #process
+    #file_path = "HW4_Vectors.csv"
+    df = pd.read_csv(filepath)
 
-    #return a numpy array
+    #process
+    targets = np.atleast_2d(np.array(df["target"])).T
+    anchors = np.atleast_2d(np.array(df["anchor"])).T
+    contexts = np.atleast_2d(np.array(df["context"])).T
+    # return a numpy array
+    nparr = np.hstack((anchors, targets, contexts))
+
+    # stack a score column if isTrain (test data has no score)
+    if isTrain:
+        score = np.atleast_2d(np.array(df["score"])).T
+        print(score.shape)
+        nparr = np.hstack((nparr, score))
+    print(nparr)
+
+    #print (nparr)
+    print (nparr)
+
+    if isTrain:
+        pass
+    return nparr
 if __name__ == "__main__":
 
     # Load data
+    #images = np.load("fashion_mnist_{}_images.npy".format(which)) / 255.
+    #labels = np.load("fashion_mnist_{}_labels.npy".format(which))
+    train = loadData("train.csv", True)   #make sure this is inside the repo on your local - it's in the gitignore
+    test = loadData("test.csv", False)  # make sure this is inside the repo on your local - it's in the gitignore
 
     # Shallow Model
 
