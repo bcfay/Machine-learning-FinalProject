6c9eac2ee7c783566eed7e921e764777ef2e206e
Jarius Thomas <jarthoma@publicisgroupe.net>
2022-05-01 16:52:04 -0400

    lining up the data process pipeline

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import numpy as np
+import time
+
+
+#Handle the input of data from test or train
+def loadData(csv)
+    #ingest a CSV
+
+    #process
+
+    #return a numpy array
 if __name__ == "__main__":
 
     # Load data
